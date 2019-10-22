2e7e737ba4dd6cdef892e7d67114aad2eb588d93
Evanny <hbpgvanny@gmail.com>
2019-10-22 18:21:54 -0500

    Actividad Secundaria, Clase de Topicos 22/10/2019

---
+++ app/src/main/java/com/example/actividadsecundaria/AcercaDe.java
@@ -0,0 +1,24 @@
+package com.example.actividadsecundaria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class AcercaDe extends AppCompatActivity {
+
+    private TextView tv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca_de);
+        tv1=(TextView)findViewById(R.id.textView);
+        Bundle bundle = getIntent().getExtras();
+        tv1.setText(bundle.getString("nombre"));
+    }
+
+    public void cerrarActivity(View view){
+        finish();
+    }
+}
